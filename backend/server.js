aa37b7d01a69482f077253a2ff19695e1005a397
Sudhanshu Vishwakarma <79822518+sudhanshuvish@users.noreply.github.com>
2021-11-22 02:05:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://sudhanshuvish:hello123@cluster0.mq6lu.mongodb.net/cluster0?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
